72a7b78cf5c7ae8e2c4560419ee5f3340914fb0a
shivam101gaur <gaurs3862@gmail.com>
2021-08-22 09:13:55 +0530

    ....

--- src/connection/socket.connect.ts
+++ src/connection/socket.connect.ts
@@ -2,12 +2,16 @@ import { Server as HttpServer } from "http"
 import { Server, Socket } from "socket.io";
 
 
-
-
 export function startSocketConnection(_httpServer: HttpServer) {
     const io = new Server(_httpServer)
     io.on("connection", (socket: Socket) => {
-        // ...
-        console.log('socket connected');
+
+        console.log('User has connected');
+        
+        socket.on('sendMessage',(message)=>{
+            console.log(message)
+        })
+
     });
 }
+
