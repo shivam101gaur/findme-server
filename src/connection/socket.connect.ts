30f54247ec5ad9482b5ef021324d224b6922747b
shivam101gaur <47584857+shivam101gaur@users.noreply.github.com>
2021-09-09 17:53:52 +0530

    naming sockets

--- src/connection/socket.connect.ts
+++ src/connection/socket.connect.ts
@@ -1,22 +1,46 @@
+import chalk from "chalk";
 import { Server as HttpServer } from "http"
 import { Server, Socket } from "socket.io";
+import { IWorld } from "../models/world.model";
 import { addMessageToWorld } from "../controllers/chat.controller";
 
 
 export function startSocketConnection(_httpServer: HttpServer) {
     const io = new Server(_httpServer)
     io.on("connection", (socket: Socket) => {
-
-        console.log('User has connected');
         
-        socket.on('msgfromuser',(letter)=>{
+         socket.on("disconnect", (reason) => {
+          console.log(chalk.yellow('user disconnected '))
+          console.log(reason)
+   
+        });
+        console.log('User has connected');
+
+        socket.on('msgfromuser', (letter) => {
+
             console.log(letter.message)
-            addMessageToWorld(letter.worldId,letter.message)
-            socket.emit("msgfromserver",{
-                content:'hello from backend to = > '+letter.worldId
-            })
+
+            addMessageToWorld(letter.worldId, letter.message).then((result) => {
+
+                console.log(result);
+                
+                if(letter.worldId==(result as any)._id){
+
+                    socket.broadcast.emit("msgfromserver", (result as any).chat)
+                }
+                
+            }).catch((err) => {
+                console.log(chalk.red(err.message));
+                socket.emit("msgfromserver", {
+                    content: 'could not send message !'
+                })
+            });
+
+
         })
 
     });
+
+
 }
 
