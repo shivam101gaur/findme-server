e9dabd360eb62d5ebcfe5a91d2c21ae1df4f82c0
shivam101gaur <gaurs3862@gmail.com>
2021-09-25 17:12:12 +0530

    ..

--- src/app.ts
+++ src/app.ts
@@ -12,19 +12,20 @@ const app = express();
 const httpServer = new HttpServer(app)
 startSocketConnection(httpServer)
 
-var allowedOrigins = ['https://find-me0.web.app/'];
+var allowedOrigins = ['https://find-me0.web.app'];
 app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin 
-        // (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
+    // origin: function (origin, callback) {
+    //     // allow requests with no origin 
+    //     // (like mobile apps or curl requests)
+    //     if (!origin) return callback(null, true);
+    //     if (allowedOrigins.indexOf(origin) === -1) {
+    //         var msg = 'The CORS policy for this site does not ' +
+    //             'allow access from the specified Origin.';
+    //         return callback(new Error(msg), false);
+    //     }
+    //     return callback(null, true);
+    // }
+    origin:allowedOrigins
 }))
 
 // middleware
